6a4cf78c1cbadc21c5f474a1588fa390b58ac03b
bezjac <mybj20@gmail.com>
2022-06-09 23:27:54 +0300

    added image directory to remote repo

--- unitTests/renderer/ProjectTest.java
+++ unitTests/renderer/ProjectTest.java
@@ -107,30 +107,31 @@ public class ProjectTest {
 
         List<LightSource> lights = new LinkedList<>();
         //lights.add(new SpotLight(new Color(255, 197, 143), new Point(145, 300, 150), new Vector(-1, -2, 0)).setkL(0.0004).setkQ(0.0000006));
+        lights.add(new SpotLight(new Color(255, 197, 143), new Point(25, 230, 950), new Vector(-0.2, -0.1, -0.5).normalize()).setkL(0.0004).setkQ(0.00006));
+       lights.add(new SpotLight( new Color(255,253,208),new Point(0,85,100),new Vector(0,-0.2,1)));
         lights.add(new SpotLight(new Color(255, 197, 143), new Point(25, 230, 950), new Vector(-0.2, -0.1, -0.5).normalize()).setkL(0.0004).setkQ(0.0000006));
-       // lights.add(new SpotLight(new Color(255, 197, 143), new Point(-125, 300, 950), new Vector(0.2, -0.1, -0.7).normalize()).setkL(0.0004).setkQ(0.0000006));
 
-        Polygon rightWall = (Polygon) new Polygon(new Point(-250, -50, 200), new Point(-250, 450, 200),
+                Polygon rightWall = (Polygon) new Polygon(new Point(-250, -50, 200), new Point(-250, 450, 200),
                 new Point(-250, 450, 1000), new Point(-250, -50, 1000)).setEmission(new Color(200,200,200));
 
         Polygon leftWall = (Polygon) new Polygon(new Point(250, -50, 200), new Point(250, 450, 200),
                 new Point(250, 450, 1000), new Point(250, -50, 1000)).setEmission(new Color(200,200,200));
         Plane solidFloor = (Plane) new Plane(new Point(0, -50, 0), new Vector(0, 1, 0)).setEmission(new Color(132, 133, 98))
-                .setMaterial(new Material().setkD(0.7).setkS(0.3).setnShininess(20));
+                .setMaterial(new Material().setkD(0.3));
         Polygon backWallTop = (Polygon) new Polygon(new Point(-250, 250, 200), new Point(-250, 450, 200), new Point(250, 450, 200), new Point(250, 250, 200))
-                .setEmission(new Color(200,200,200));
+                .setEmission(new Color(200,192,205));
         Polygon backWallBottom = (Polygon) new Polygon(new Point(-250, -50, 200), new Point(-250, 50, 200), new Point(250, 50, 200), new Point(250, -50, 200))
-                .setEmission(new Color(200,200,200));
+                .setEmission(new Color(200,192,205));
         Polygon backWallLeft = (Polygon) new Polygon(new Point(-250, 50, 200), new Point(-250, 250, 200), new Point(-150, 250, 200), new Point(-150, 50, 200))
-                .setEmission(new Color(200,200,200));
+                .setEmission(new Color(200,192,205));
         Polygon backWallRight = (Polygon) new Polygon(new Point(150, 50, 200), new Point(150, 250, 200), new Point(250, 250, 200), new Point(250, 50, 200))
-                .setEmission(new Color(200,200,200));
+                .setEmission(new Color(200,192,205));
 
 
 
 
-        FloorSquare floor = new FloorSquare(new Point(-250, -50, 200), Vector.Z_AXIS, Vector.X_AXIS, new Color(188,158,130),
-                new Color(84, 57, 38), 1200, 500, 36, 15,new Material().setkD(0.3).setnShininess(30).setkS(0.3));
+        FloorSquare floor = new FloorSquare(new Point(-250, -50, 200), Vector.Z_AXIS, Vector.X_AXIS, new Color(129, 223, 176),
+                new Color(112,147,158), 1200, 500, 36, 15,new Material().setkD(0.3));
 
         Polygon window = (Polygon) new Polygon(new Point(-150, 50, 60), new Point(-150, 250, 60), new Point(150, 250, 60), new Point(150, 50, 60))
                 .setMaterial(new Material().setkT(0.78).setkD(0.22));
@@ -190,7 +191,7 @@ public class ProjectTest {
                 Vector.X_AXIS, Vector.Z_AXIS, new Color(84, 57, 38), false)
                 .setSeatMaterial(new Material().setkD(0.7).setkS(0.05));
           Table table = new Table(75, 95d, new Color(84, 57, 38), new Point(-8, -50, 850), new Vector(0, 1, 0), new Vector(1, 0, -1))
-                .setMaterialSurfaceTop(new Material().setkS(0.05).setkD(0.3)).setColorBars(new Color(186, 135, 89));
+                .setMaterialSurfaceTop(new Material().setkS(0.05).setnShininess(20).setkD(0.3)).setColorBars(new Color(186, 135, 89));
 
 
         Scene scene = new Scene.SceneBuilder("Test Scene")
@@ -202,8 +203,8 @@ public class ProjectTest {
                         backWallBottom,
                         backWallLeft,
                         backWallRight,
-                        solidFloor,
-                       //floor.elements,
+                       // solidFloor,
+                       floor.elements,
                         window,
                         frameHorizontalTop,
                         frameHorizontalBottom,
@@ -219,7 +220,7 @@ public class ProjectTest {
                        leftChair.getGeometries(),
                         rightChair.getGeometries(),
                         frontChair.getGeometries(),
-                        backChair.getGeometries(),
+                       // backChair.getGeometries(),
                         //cornerChair1.getGeometries(),
                      //   cornerChair.getGeometries(),
                      //   cornerChairFlipped1.getGeometries(),
@@ -230,13 +231,14 @@ public class ProjectTest {
                 .setBackground(new Color(0, 102d, 102d))
                 .build();
 
-        ImageWriter imageWriter = new ImageWriter("ProjectTest8Adaptive", 600, 600);
-        Camera camera = new Camera.CameraBuilder(new Point(0, 130, 1400), new Vector(0, -0.15, -1), new Vector(0, (double) 20 / 3, -1)) //
+        ImageWriter imageWriter = new ImageWriter("ProjectTest9", 600, 600);
+        Camera camera = new Camera.CameraBuilder(new Point(150, 130, 1400), new Vector(-0.2, -0.15, -1), new Vector(0, (double) 20 / 3, -1)) //
                 .setVPSize(600, 600)
                 .setVPDistance(1000)
                 .setImageWriter(imageWriter) //
                 .setRayTracer(new RayTracerBasic(scene))
-                .setAntiAliasing(AntiAliasing.ADAPTIVE).setRecurseDepth(8)
+                .setAntiAliasing(AntiAliasing.NONE)
+                .setMultithreading(3)
                 .setDebugPrint(0.1)
                 .build();//
 
